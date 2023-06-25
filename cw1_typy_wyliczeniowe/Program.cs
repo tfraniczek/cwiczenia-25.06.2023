ced9ab49f01a75ccba5338f1648a9fb9b8605e6b
tfraniczek <tymoteusz.franiczek@gmail.com>
2023-06-25 10:01:14 +0200

    1

--- cw1_typy_wyliczeniowe/Program.cs
+++ cw1_typy_wyliczeniowe/Program.cs
@@ -6,6 +6,40 @@ using System.Threading.Tasks;
 
 namespace cw1_typy_wyliczeniowe
 {
+    enum DniTygodnia
+    {
+        Poniedziałek,
+        Wtorek,
+        Środa,
+        Czwartek,
+        Piątek,
+        Sobota,
+        Niedziela
+    }
+    enum EtapyPrania 
+    {
+        Pranie,
+        Plukanie,
+        Wirowanie,
+        Suszenie
+    }
+    enum Posilki
+    {
+        Pierwsze_Sniadanie,
+        Drugie_Sniadanie,
+        Lunch,
+        Obiac,
+        Kolacja
+    }
+    enum BierkiSzachowe
+    {
+        Krol,
+        Hetman,
+        Goniec,
+        Skoczek,
+        Wieza,
+        Pionek
+    }
     internal class Program
     {
         static void Main(string[] args)
