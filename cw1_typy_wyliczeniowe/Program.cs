fe5f3b95e8644de928aaa03a69c406d074ebb9d5
tfraniczek <tymoteusz.franiczek@gmail.com>
2023-06-25 10:00:29 +0200

    1

---
+++ cw1_typy_wyliczeniowe/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw1_typy_wyliczeniowe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
